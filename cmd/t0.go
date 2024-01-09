a77ee8ab620e7f66cc5cf1abb913d30274beac26
David MICHENEAU <david.micheneau@orange.com>
2024-01-09 15:48:04 +0100

    feat: improve printed result

--- cmd/t0.go
+++ cmd/t0.go
@@ -4,9 +4,11 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"text/tabwriter"
 
-	jsontmpl "github.com/orange-cloudavenue/cloudavenue-cli/pkg/templates/json"
 	"github.com/spf13/cobra"
 )
 
@@ -39,28 +41,37 @@ var t0ListCmd = &cobra.Command{
 			return
 		}
 
-		// Struct to print a basic view
-		type basict0 = struct {
-			T0Vrf          string `json:"t0_vrf"`
-			T0Provider     string `json:"t0_provider"`
-			T0ClassService string `json:"t0_class_service"`
-		}
-		basict0s := []*basict0{}
-
-		// Set the struct
-		for _, t0 := range *t0s {
-			x := &basict0{
-				T0Vrf:          t0.Tier0Vrf,
-				T0Provider:     t0.Tier0Provider,
-				T0ClassService: t0.Tier0ClassService,
-			}
-			basict0s = append(basict0s, x)
-		}
-
 		// Print the result
-		jsontmpl.Format(jsontmpl.JsonTemplate{
-			Fields: []string{"t0_vrf", "t0_provider", "t0_class_service"},
-			Data:   basict0s,
-		})
+		w := tabwriter.NewWriter(os.Stdout, 10, 1, 5, ' ', 0)
+		fs := "%s\t%s\t%s\n"
+		fmt.Fprintf(w, fs, "NAME", "PROVIDER", "CLASS_SERVICE")
+		for _, t := range *t0s {
+			fmt.Fprintf(w, fs, t.Tier0Vrf, t.Tier0Provider, t.Tier0ClassService)
+		}
+		w.Flush()
+
+		// // Struct to print a basic view
+		// type basict0 = struct {
+		// 	T0Vrf          string `json:"t0_vrf"`
+		// 	T0Provider     string `json:"t0_provider"`
+		// 	T0ClassService string `json:"t0_class_service"`
+		// }
+		// basict0s := []*basict0{}
+
+		// // Set the struct
+		// for _, t0 := range *t0s {
+		// 	x := &basict0{
+		// 		T0Vrf:          t0.Tier0Vrf,
+		// 		T0Provider:     t0.Tier0Provider,
+		// 		T0ClassService: t0.Tier0ClassService,
+		// 	}
+		// 	basict0s = append(basict0s, x)
+		// }
+
+		// // Print the result
+		// jsontmpl.Format(jsontmpl.JsonTemplate{
+		// 	Fields: []string{"t0_vrf", "t0_provider", "t0_class_service"},
+		// 	Data:   basict0s,
+		// })
 	},
 }
